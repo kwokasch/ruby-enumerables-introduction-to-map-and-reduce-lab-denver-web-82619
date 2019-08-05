0bce1cff0e47bf7ab58f11488405c07dc5c53baa
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-05 15:08:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,9 @@
 # My Code here....
 def map_to_negativize (source_array)
-  source_array.map(|i| i*-1)
+  i = 0 
+  while i < source_array.length do
+    source_array.map(|i| i*-1)
   return Array
+  end 
+  i+=1 
 end 
